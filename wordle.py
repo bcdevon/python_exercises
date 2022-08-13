ae89b32fd44b7d8115cb4045f0648d04cd68720c
bcdevon <brady.devon0@gmail.com>
2022-08-12 17:28:25 -0700

    gitignore

---
+++ wordle.py
@@ -0,0 +1,22 @@
+def evaluate_word(guess, answer):
+    """Given a word and an answer, evalutate the accuracy of the guess like world.
+
+    X means wrong letter and place.
+    ? means wrong place.
+    ! means right letter and place.
+
+    Args:
+        guess:
+            A string representing the guess.
+        answer:
+            A string representing the answer.
+
+    Return:
+        A string reflecting the accuracy of the guess. Example:
+
+        guess =  'abcde'
+        answer = 'zxdce'
+        result = 'XX??!'
+    """
+
+    return ""
