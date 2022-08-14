ef632ff633184cd60474bcafe19272e122133cc5
bcdevon <brady.devon0@gmail.com>
2022-08-14 15:22:12 -0700

    restaurant

---
+++ chapter 9/restaurant.py
@@ -0,0 +1,22 @@
+class Restaurant:
+
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(f"{self.restaurant_name} serves {self.cuisine_type} food.")
+
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} is open.")
+
+
+new_restaurant = Restaurant('evangelos', 'italian')
+my_food = Restaurant('mcdonalds', 'american')
+your_food = Restaurant('KFC', 'southern')
+print(f"{new_restaurant.restaurant_name}")
+print(f"{new_restaurant.cuisine_type}")
+new_restaurant.describe_restaurant()
+my_food.describe_restaurant()
+your_food.describe_restaurant()
+new_restaurant.open_restaurant()
