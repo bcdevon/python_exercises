b1ee2d8c9e1ad1d8859ec36454f0d03e82f81c1e
bcdevon <brady.devon0@gmail.com>
2022-10-25 18:18:28 -0700

    inorder_traversal

--- chapter 9/inorder_traversal.py
+++ chapter 9/inorder_traversal.py
@@ -18,6 +18,22 @@ root.left.right = TreeNode(9)
 root.right.left = TreeNode(13)
 root.right.right = TreeNode(6)
 
+root_a = TreeNode(31)
+root_a.left = TreeNode(15)
+root_a.right = TreeNode(120)
+root_a.left.left = TreeNode(21)
+root_a.left.right = TreeNode(39)
+root_a.right.left = TreeNode(13)
+root_a.right.right = TreeNode(6)
+
+root_b = TreeNode(3)
+root_b.left = TreeNode(5)
+root_b.right = TreeNode(10)
+root_b.left.left = TreeNode(2)
+root_b.left.right = TreeNode(9)
+root_b.right.left = TreeNode(13)
+root_b.right.right = TreeNode(6)
+
 def traverse_tree_inorder(root):
     """Given the root, traverse the binary tree inorder, creating a list 
     of the nodes (or node values). 
@@ -40,5 +56,37 @@ def traverse_tree_inorder(root):
             Image a tree: https://assets.leetcode.com/uploads/2020/09/15/inorder_1.jpg
             return [1,3,2]
     """
+    # res = []
+    # stack = []
+    # cur = root
+    # while cur or stack:
+    #     while cur:
+    #         stack.append(cur)
+    #         cur = cur.left
+    #     cur = stack.pop()
+    #     res.append(cur.val)
+    #     cur = cur.right
+    # print(res)
+    stack = []
+    compare = []
+    iter = root
 
-    return []
+    while iter is not None or len(stack) > 0:
+        while iter is not None:
+            stack.append(iter)
+            iter = iter.left
+        iter = stack.pop()
+        compare.append(iter.val)
+        iter = iter.right
+    return compare
+
+
+def compare_trees(root_a, root_b):
+    stack_a = traverse_tree_inorder(root_a)
+    stack_b = traverse_tree_inorder(root_b)
+    if stack_a == stack_b:
+        print("True")
+    else:
+        print("False")
+
+compare_trees(root, root_b)
