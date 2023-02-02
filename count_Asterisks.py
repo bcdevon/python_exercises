d94db3596d80f45c98f73aea77a75eb128ef13e8
bcdevon <brady.devon0@gmail.com>
2023-02-01 18:12:07 -0800

    leetcode count Asterisks

---
+++ count_Asterisks.py
@@ -0,0 +1,11 @@
+                    
+s = "yo|uar|e**|b|e***au|tifu|l"
+pair_count = 0
+count = 0
+for i in s:
+    if i == "|":
+        pair_count += 1
+    if pair_count%2 == 0 and i == "*":
+        count += 1
+print(count)
+
